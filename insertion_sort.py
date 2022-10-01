42ad6c4bbd77b2ab9efd189b98b4036678097e4d
Epicman25 <Epicman25@gmail.com>
2022-10-01 08:34:32 +0530

    Added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+    return arr
+
+if __name__ == '__main__':
+    arr = [5, 4, 3, 2, 1]
+    print(insertion_sort(arr))  
